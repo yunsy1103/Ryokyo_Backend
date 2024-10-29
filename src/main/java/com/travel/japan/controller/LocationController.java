0d9c54d1f03ecbda171f83094983e307b0c6120d
yunsy1103 <yunsy1103@naver.com>
2024-10-29 16:40:59 +0900

    위도,경도 Put 매핑

--- src/main/java/com/travel/japan/controller/LocationController.java
+++ src/main/java/com/travel/japan/controller/LocationController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -24,7 +21,7 @@ public class LocationController {
     @Autowired
     private MemberRepository memberRepository;
 
-    @PostMapping("/save")
+    @PutMapping("/save")
     public ResponseEntity<String> saveLocation(@RequestBody LocationRequest locationRequest) {
         System.out.println("saveLocation endpoint reached");
         double latitude = locationRequest.getLatitude();
